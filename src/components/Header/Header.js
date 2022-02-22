78cbc75d25ecf4e7554d9e294a41d20b4ac57d05
Bobokhon <rboboxon6055@gmail.com>
2022-02-22 13:17:16 +0900

    bugs fixed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -32,7 +32,7 @@ const Header = () => (
       </li>
     </Div2>
     <Div3>
-      <SocialIcons href="https://github.com/Boboxon">
+      <SocialIcons href="https://github.com/bobokhon7">
         <AiFillGithub size="3rem" />
       </SocialIcons>
       <SocialIcons href="https://www.linkedin.com/in/bobokhon/">
